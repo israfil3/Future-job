84997cac560992d47cd6d6b73f6533c6b62a62ce
unknown <mdisrafil61@gmail.com>
2023-04-12 22:55:22 +0600

    new data add

--- src/component/newBtn/NewBtn.js
+++ src/component/newBtn/NewBtn.js
@@ -12,41 +12,41 @@ const NewBtn = () => {
     //     </div>
     // );
     return (
-        <div className='maxw mt-20 p-40'>
+        <div className='maxw mt-20 p-24'>
             {
               detailsdata.map((data)=>{
                 console.log(data.company_name)
-               const  {id,company_name,Job_time_2,logo_url,Job_time_1,Salary,job_post,location,phone,email}= data;
+               const  {id,company_name,Job_time_2,logo_url,Job_time_1,Salary,job_post,location,phone,email,Address,description,Educational_Requirements,Experiences,Job_Responsibility}= data;
                 // const {addToAppliedList,appliedlist} = useContext(GlobalContext)
             //    let storeApplied = data.find((exits)=>exits.id===idd);
             //    console.log(storeApplied);
             console.log(idd)
                 if(data.id==idd){
-                    return <><h1 className='text-center text-3xl mb-14'>JobDetails</h1> 
+                    return <><h1 className='text-center text-3xl m-14'>JobDetails</h1> 
                     <div className='grid grid-cols-2 gap-24' key={data.id}>
                         <div>
-                            <p>jobDescription:{data.jobDescription}</p><br></br>
-                            <p>jobResponsibilities: {data.jobResponsibilities}</p><br></br>
-                            <p>EducationalRequirements {data.educationalRequirements}</p>
-                            <p>Experiences: {data.experiences}</p>
+                            <p><span className='text-2xl'>JobDescription</span>:{description}</p><br></br>
+                            <p><span className='text-2xl'>jobResponsibilities</span>: {Job_Responsibility}</p><br></br>
+                            <p><span className='text-2xl'>EducationalRequirements</span>:{Educational_Requirements}</p>
+                            <p><span className='text-2xl'>Experiences</span>:{Experiences}</p>
                         </div>
-                        <div className='flex flex-col gap-3'>
+                        <div className='flex flex-col gap-3 bg-violet-50 p-10 rounded-lg'>
                             <h2 className='text-2xl'>Job details</h2>
                            <div className='flex'> 
                            <p>salary:<span>{data.Salary}per-month</span></p>
                            </div>
-                           <div className='flex'> 
+                           <div className='flex '> 
                            <p>jobTitle:<span>{job_post}</span></p>
                            </div>
                            <h1 className='text-2xl'> Contact information</h1>
                            <div className='flex'> 
                            <p>Phone:<span>{phone}</span></p>
                            </div>
-                           <div className='flex'> 
+                           <div className='flex '> 
                            <p>Email:<span>{email}</span></p>
                            </div>
                            <div className='flex'> 
-                           <p>Address:</p><img src={logo_url} alt='img'></img><span></span>
+                           <p>Address:</p><span>Address</span>
                            </div>
                            <button className='btn mt-10'> Apply Now</button>
                         </div>
